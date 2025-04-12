edd8a63ef74049f1316f7884922e665f425ba037
santiagomary1004 <22620262@tlaxiaco.tecnm.mx>
2025-04-11 20:00:23 -0600

    Agregando ruta de los componentes

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,98 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Proyectos</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <router-link to="/about">About</router-link>
+          </a>
+        </li>
+
         <li class="nav-item">
           <a class="nav-link" href="#">
             <router-link to="/sistema">Sistema</router-link>
           </a>
         </li>
+
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" 
+          aria-expanded="false">
+            Card
+          </a>
+          <ul class="dropdown-menu">
+            <li class="nav-item">
+          <a class="nav-link" href="#">
+            <router-link to="/card1">Card</router-link>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <router-link to="/card">Card1</router-link>
+          </a>
+        </li>
+      </ul>
+    </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <router-link to="/pagina">Pizzas</router-link>
+          </a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" 
+          aria-expanded="false">
+            Datos
+          </a>
+          <ul class="dropdown-menu">
+            <li>
+              <a class="dropdown-item" href="#">
+                <router-link to="/recorrido">Recorrido</router-link>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">
+                <router-link to="/recorrido1">Recorrido1</router-link>
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <router-link to="/horario">Horario</router-link>
+          </a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" 
+          aria-expanded="false">
+            Props
+          </a>
+          <ul class="dropdown-menu">
+            <li>
+              <a class="dropdown-item" href="#">
+                <router-link to="/padre">Props</router-link>
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <router-link to="/eventos">Eventos</router-link>
+          </a>
+        </li>
+            
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" 
+          aria-expanded="false">
             Calculadora
           </a>
           <ul class="dropdown-menu">
@@ -35,93 +115,22 @@
         </li>
           </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <router-link to="/pagina">Pizzas</router-link>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <router-link to="/about">About</router-link>
-          </a>
-        </li>
         
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Card
-          </a>
-          <ul class="dropdown-menu">
-            <li class="nav-item">
-          <a class="nav-link" href="#">
-            <router-link to="/card1">Card</router-link>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <router-link to="/card">Card1</router-link>
-          </a>
-        </li>
-            
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Datos
-          </a>
-          <ul class="dropdown-menu">
-            <li>
-              <a class="dropdown-item" href="#">
-                <router-link to="/recorrido">Recorrido</router-link>
-              </a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">
-                <router-link to="/recorrido1">Recorrido1</router-link>
-              </a>
-            </li>
-            
-          </ul>
-        </li>
         
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Props
-          </a>
-          <ul class="dropdown-menu">
-            <li>
-              <a class="dropdown-item" href="#">
-                <router-link to="/padre">Props</router-link>
-              </a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">
-                <router-link to="/recorrido1">Recorrido1</router-link>
-              </a>
-            </li>
-            
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <router-link to="/horario">Horario</router-link>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <router-link to="/eventos">Eventos</router-link>
-          </a>
-        </li>
+        
+        
+        
+        
+        
+
+        
+
+        
       </ul>
     </div>
   </div>
 </nav>
-  <!-- <h1>Vue Router</h1> -->
-  <!-- <nav>
-    
-    <router-link to="/about">About</router-link>
-  </nav> -->
-  <router-view></router-view>
-</div>
+  </div>
 </div>
 </template>
 
